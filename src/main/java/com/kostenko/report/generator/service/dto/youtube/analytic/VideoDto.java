7b62bcc91526341e6e4ff1aded4899a01e1462e7
Roman Kostenko <romen.kost.2332@gmail.com>
2022-02-21 10:38:33 +0200

    Fixed DTOs.

--- src/main/java/com/kostenko/report/generator/service/dto/youtube/analytic/VideoDto.java
+++ src/main/java/com/kostenko/report/generator/service/dto/youtube/analytic/VideoDto.java
@@ -1,13 +1,21 @@
 package com.kostenko.report.generator.service.dto.youtube.analytic;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class VideoDto {
+    @JsonProperty("videoId")
     private String videoId;
+
+    @JsonProperty("title")
     private String title;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("publishedAt")
     private Date publishedAt;
 }
